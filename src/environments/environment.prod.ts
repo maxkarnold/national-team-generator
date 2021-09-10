56a9f83ac86a590ff5f0690ae03699a374c8e86a
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2021-09-10 09:33:01 -0500

    removed /secrets and /environments from git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7Cxl8eSGaYlQUwXOGfOUiKsoeUXJnX2g",
+    authDomain: "national-team-generator.firebaseapp.com",
+    projectId: "national-team-generator",
+    storageBucket: "national-team-generator.appspot.com",
+    messagingSenderId: "30261669176",
+    appId: "1:30261669176:web:eca1c9103db91bab6265cd",
+    measurementId: "G-QV0DJSQSL2"
+  }
+};
