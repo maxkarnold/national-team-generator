5bf9932445ba27bd063414c0bfe20089094037e2
maxkarnold <60246034+maxkarnold@users.noreply.github.com>
2022-02-01 11:33:03 -0600

    added registration capability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,32 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'team-gen';
   loginOverlayOpen = false;
+  registerOverlayOpen = false;
   navToggle = false;
   isLoggedIn = false;
-  
-  subscription: Subscription = new Subscription;
 
-  constructor (private auth: AuthService, private cd: ChangeDetectorRef ) {
+  subscription: Subscription = new Subscription();
 
-  }
+  constructor(
+    private afs: AngularFirestore,
+    private auth: AuthService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.auth.currentAuthState.subscribe(authState => this.isLoggedIn = authState);
+    this.subscription = this.auth.currentAuthState.subscribe(
+      (authState) => (this.isLoggedIn = authState)
+    );
     if (localStorage.getItem('user') !== null) {
       this.auth.changeAuthState(true);
     } else {
@@ -31,35 +37,80 @@ export class AppComponent implements OnInit {
   loginOverlay() {
     if (!this.loginOverlayOpen) {
       this.loginOverlayOpen = true;
+      this.registerOverlayOpen = false;
     } else {
       this.loginOverlayOpen = false;
     }
   }
 
+  registerOverlay() {
+    if (!this.registerOverlayOpen) {
+      this.registerOverlayOpen = true;
+      this.loginOverlayOpen = false;
+    } else {
+      this.registerOverlayOpen = false;
+    }
+  }
+
   login(email: string, password: string) {
-    this.auth.login(email, password)
-      .then(res => {
+    this.auth
+      .login(email, password)
+      .then((res) => {
         this.auth.changeAuthState(true);
         localStorage.setItem('user', JSON.stringify(res.user));
         this.loginOverlayOpen = false;
         console.log('Logged in');
       })
-      .catch(err => {
-        var errorCode = err.code;
-        var errorMessage = err.message;
+      .catch((err) => {
+        let errorCode = err.code;
+        let errorMessage = err.message;
         if (errorCode === 'auth/wrong-password') {
           alert('Wrong password.');
         } else {
           alert(errorMessage);
         }
         console.log(err);
-      }); 
+      });
   }
-  
+
+  signup(name: string, email: string, password: string) {
+    this.auth
+      .signUp(email, password)
+      .then((res) => {
+        this.auth.changeAuthState(true);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this.registerOverlayOpen = false;
+        console.log('Registered Account');
+        alert('Succesfully registered and logged in!');
+      })
+      .catch((err) => {
+        let errorCode = err.code;
+        let errorMessage = err.message;
+        if (errorCode === 'auth/email-already-in-use') {
+          alert('This email is already in use.');
+        } else if (errorCode === 'auth/invalid-email') {
+          alert('This email address is invalid.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(err);
+      });
+    this.auth.getUser().subscribe((user) => {
+      if (user) {
+        this.afs.collection('users').doc(user.uid).set({
+          email: email,
+          userId: user.uid,
+          displayName: name,
+        });
+      } else {
+        console.log('error when signing up');
+      }
+    });
+  }
+
   logout() {
     this.auth.logout();
     console.log('logged out');
     this.auth.changeAuthState(false);
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,38 +4,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private authStateSource = new BehaviorSubject<boolean>(false);
   currentAuthState = this.authStateSource.asObservable();
-  
-  
-  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore) { }
+
+  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore) {}
 
   login(email: string, password: string) {
-    return this.afAuth.signInWithEmailAndPassword(email, password)
-      
+    return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
-  async signUp(email: string, password: string) {
-    await this.afAuth.createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        this.changeAuthState(true);
-        localStorage.setItem('user', JSON.stringify(res.user));
-      });
-    this.getUser().subscribe((user) => {
-      if (user) {
-        this.afs.collection('users').doc(user.uid).set({
-          email: email,
-          userId: user.uid
-        });
-      } else {
-        console.log('error when signing up');
-      }
-    })
-    
+  signUp(email: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
   }
 
   logout() {
@@ -46,12 +28,10 @@ export class AuthService {
 
   getUser() {
     const user$ = this.afAuth.user;
-    return user$
+    return user$;
   }
 
   changeAuthState(authState: boolean) {
     this.authStateSource.next(authState);
   }
-  
-
 }
